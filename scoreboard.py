93488b62a4f4780f78d3ae7f8ba1eed15012d979
muziriyueliming <muziriyuelliming@gmail.com>
2021-05-20 23:45:38 +0800

    显示等级2

--- scoreboard.py
+++ scoreboard.py
@@ -21,7 +21,7 @@ class Scoreboard():
 	def prep_score(self):
 		"""将得分转换为一副渲染的图像。"""
 		rounded_score = round(self.stats.score, -1)
-		score_str = "{:,}".format(rounded_score)
+		score_str = "Score: " + "{:,}".format(rounded_score)
 		self.score_image = self.font.render(score_str, True,
 				self.text_color, self.settings.bg_color)
 
@@ -33,7 +33,7 @@ class Scoreboard():
 	def prep_high_score(self):
 		"""将最高得分转换为渲染的图像。"""
 		high_score = round(self.stats.high_score, -1)
-		high_score_str = "{:,}".format(high_score)
+		high_score_str = "High Score: " + "{:,}".format(high_score)
 		self.high_score_image = self.font.render(high_score_str, True,
 				self.text_color, self.settings.bg_color)
 
@@ -41,6 +41,17 @@ class Scoreboard():
 		self.high_score_rect = self.high_score_image.get_rect()
 		self.high_score_rect.centerx = self.screen_rect.centerx
 		self.high_score_rect.top = self.score_rect.top 
+
+	def prep_level(self):
+		"""将等级转换为渲染的图像。"""
+		level_str =  "Level: " + str(self.stats.level)
+		self.level_image = self.font.render(level_str, True,
+			self.text_color, self.settings.bg_color)
+
+		# 将等级放到得分下方。
+		self.level_rect = self.level_image.get_rect()
+		self.level_rect.right = self.score_rect.right
+		self.level_rect.top = self.score_rect.bottom + 10
 
 	def show_score(self):
 		"""在屏幕上显示得分。"""
@@ -54,13 +65,3 @@ class Scoreboard():
 			self.stats.high_score = self.stats.score
 			self.prep_high_score()
 
-	def prep_level(self):
-		"""将等级转换为渲染的图像。"""
-		level_str = str(self.stats.level)
-		self.level_image = self.font.render(level_str, True,
-			self.text_color, self.settings.bg_color)
-
-		# 将等级放到得分下方。
-		self.level_rect = self.level_image.get_rect()
-		self.level_rect.right = self.score_rect.right
-		self.level_rect.top = self.score_rect.bottom + 10
